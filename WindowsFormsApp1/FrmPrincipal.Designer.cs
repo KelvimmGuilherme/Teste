cd7a049fd246ee52a7742fca46226414e81d73e2
Kelvim <Kelvim@DESKTOP-3ERNKSQ>
2025-01-16 20:46:57 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsApp1/FrmPrincipal.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btmMensagem = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btmMensagem
+            // 
+            this.btmMensagem.Location = new System.Drawing.Point(250, 142);
+            this.btmMensagem.Name = "btmMensagem";
+            this.btmMensagem.Size = new System.Drawing.Size(229, 109);
+            this.btmMensagem.TabIndex = 0;
+            this.btmMensagem.Text = "button1";
+            this.btmMensagem.UseVisualStyleBackColor = true;
+            this.btmMensagem.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btmMensagem);
+            this.Name = "FrmPrincipal";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btmMensagem;
+    }
+}
+
---
+++ WindowsFormsApp1/FrmPrincipal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            minhaBiblioteca.minhaCLasse minhaCLasse = new minhaBiblioteca.minhaCLasse();
+            // introduzindo minhaBiblioteca na classe do botão para utiliza-la
+            string Texto = "Oiii";
+             System.Windows.Forms.MessageBox.Show("Mensagem do botão");
+            //menssagem do botão
+            MessageBox.Show(Texto); //utilizando messageBox utilizando a string Texto
+            MessageBox.Show(minhaCLasse.name);//utilizando a string armazenada no nameSpace
+        }
+    }
+}
+
+    namespace minhaBiblioteca //criando namespace
+{
+    public class minhaCLasse // definindo a classe
+    {
+        public string name = "ta salvado"; //introduzindo uma string
+    }
+}
+    
